f9601b6c3a6171bc7cfb3a428818fe1d3ba79f64
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-05-23 15:10:08 +0200

    feat: read dati json

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+const DB_FILE = "norrisDb";
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.end(`<h1>ciao</h1>`);
-  })
-  .listen(port, host, () => {
-    const serverUrl = `http://${host}:${port}`;
-    console.log(`Server avviato su ${serverUrl}`);
-  });
+// lettura dati JSON
+const readJSONData = (nomeFile) => {
+  const filePath = path.join(__dirname, nomeFile + ".json");
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, "utf-8");
+    if (fileData) {
+      return JSON.parse(fileData);
+    }
+  }
+  return [];
+};
